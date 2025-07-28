f6c1b257f3132da1bb1404b12113c0311ae83f7d
Noorain Fathima <57002581+Noorain-spec@users.noreply.github.com>
2025-07-29 00:00:27 +0530

    Update LeftRotate.java

--- Arrays/LeftRotate.java
+++ Arrays/LeftRotate.java
@@ -1,5 +1,5 @@
 /*
-Given an array of N integers, left rotate the array by one place.
+Given an array of N integers, left rotate the array by N place.
 
 Input: N = 5, array[] = {1,2,3,4,5}
 Output: 2,3,4,5,1
